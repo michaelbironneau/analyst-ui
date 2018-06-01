f2c91efc701055069acf50cb60bb8afd78dac1fd
Michael Bironneau <michael.bironneau@openenergi.com>
2018-06-01 16:11:40 +0100

    Improve experience when adding AQL task; improve log view

--- src/app/views/scheduler/new-task.component.ts
+++ src/app/views/scheduler/new-task.component.ts
@@ -11,6 +11,12 @@ import { SchedulerService } from './scheduler.service';
   templateUrl: 'new-task.component.html'
 })
 export class NewTaskComponent {
+  private showAddOption: Boolean = false;
+  private newAQLOption = {
+    name: '',
+    value: null,
+    type: 'String'
+  }
   private blank_task = {
     id: null,
     name: '',
@@ -30,12 +36,50 @@ export class NewTaskComponent {
     args: '',
     schedule: '@daily'
   };
+  private aqlOptions = [];
 
   constructor(private ss: SchedulerService, private router: Router){
    ;
   }
+ 
+  deleteOption(i){
+    this.aqlOptions.splice(i, 1);  
+  }
+  
+  addOption(){
+    const newOption = JSON.parse(JSON.stringify(this.newAQLOption));
+    if (newOption.type == 'Number'){
+      newOption.value = parseFloat(newOption.value);
+    }
+    let found = false;
+    for (var i=0; i<this.aqlOptions.length; i++){
+      if (this.aqlOptions[i].name.toLowerCase()==newOption.name.toLowerCase()){
+        this.aqlOptions[i] = newOption;
+        found = true;
+        break;
+      }
+    }
+    if (!found) this.aqlOptions.push(newOption);
+    this.newAQLOption = {
+      name: '',
+      value: null,
+      type: 'String'
+    };
+  }
+  
+  makeAQLArgs(){
+    if (this.aqlOptions.length==0) return '';
+    var args = {};
+    this.aqlOptions.forEach(opt => {
+      args[opt.name] = opt.value;
+    });
+    return JSON.stringify(args);
+  }
   
   create(){
+    if (this.task.is_aql){
+      this.task.args = this.makeAQLArgs();
+    }
     this.ss.createTask(this.task).subscribe(resp => {
       this.task = JSON.parse(JSON.stringify(this.blank_task));
       this.router.navigate(['/scheduler/tasks']);
